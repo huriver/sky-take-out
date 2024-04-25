8a6f970bf642c75fadbf0ef71926de47f53588d2
huriver <2195445402@qq.com>
2024-04-25 10:24:44 +0800

    新增菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,6 +1,11 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
@@ -8,10 +13,23 @@ public interface DishMapper {
 
     /**
      * 根据分类id查询菜品数量
+     *
      * @param categoryId
      * @return
      */
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
 
+    /**
+     * 插入菜品数据
+     *
+     * @param dish
+     */
+    @AutoFill(value = OperationType.INSERT)
+    @Insert("insert into dish (name, category_id, price, image, description, status, create_time, update_time," +
+            " create_user, update_user)" +
+            " values (#{name},#{categoryId},#{price},#{image},#{description},#{status},#{createTime},#{updateTime}," +
+            "#{createUser},#{updateUser})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void insert(Dish dish);
 }
