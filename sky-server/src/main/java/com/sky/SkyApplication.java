15d2bf403ef5d90b010da9330c4ca30203061a97
huriver <2195445402@qq.com>
2024-05-05 16:05:36 +0800

    避免Druid异常警告

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -9,6 +9,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
 public class SkyApplication {
+
+    //避免Druid异常警告 discard long time none received connection. 问题
+    static {
+        System.setProperty("druid.mysql.usePingMethod", "false");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
         log.info("server started");
